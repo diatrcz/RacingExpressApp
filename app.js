de09d1b106502b1b34b8cf7e3a4bcd9fd322dbb7
Tarcza Lídia <100163235+diatrcz@users.noreply.github.com>
2024-09-27 18:41:44 +0200

    Add basic UI

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//app.set('view engine', 'ejs');
+//app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('views'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
